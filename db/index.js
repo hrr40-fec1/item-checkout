ba208fbb91b17e25895c7db107e66acb3d242ecc
AndrewTjandra <andrew.tjandra@gmail.com>
2019-08-23 14:41:39 -0700

    Implement Express Server with API routes

---
+++ db/index.js
@@ -0,0 +1,74 @@
+const db = require('mongoose');
+require('mongoose-currency').loadType(db);
+
+const { Currency } = db.Types;
+
+db.connect('mongodb://localhost:27017/ontarget', { useNewUrlParser: true });
+
+db.connection.on('connected', () => {
+  // eslint-disable-next-line no-console
+  console.log('connected to MongoDB');
+});
+
+db.connection.on('error', (err) => {
+  // eslint-disable-next-line no-console
+  console.log('Error occurred: ', err);
+});
+
+/*
+  Schema Definition
+*/
+const reviewSchema = db.Schema({
+  rating: Number,
+});
+
+const productSchema = db.Schema({
+  productId: Number,
+  name: String,
+  price: { type: Currency },
+  size: [String],
+  color: {},
+  reviews: [reviewSchema],
+});
+
+const inventorySchema = db.Schema({
+  productId: Number,
+  size: String,
+  color: String,
+  storeId: Number,
+  quantity: Number,
+});
+
+/*
+  Model Definition
+*/
+const Products = db.model('products', productSchema);
+const Inventory = db.model('inventory', inventorySchema, 'inventory');
+
+/*
+  Function Definitions
+*/
+
+const getProduct = (productId) => new Promise((resolve, reject) => {
+  Products.find({ productId })
+    .then((product) => {
+      resolve(product);
+    })
+    .catch((err) => {
+      reject(err);
+    });
+});
+
+const getQuantity = (productId, color, size, storeId) => new Promise((resolve, reject) => {
+  // eslint-disable-next-line object-curly-newline
+  Inventory.find({ productId, color, size, storeId })
+    .then((result) => {
+      resolve(result[0].quantity);
+    })
+    .catch((err) => {
+      reject(err);
+    });
+});
+
+module.exports.getProduct = getProduct;
+module.exports.getQuantity = getQuantity;
--- server/index.js
+++ server/index.js
@@ -1,7 +1,33 @@
 const express = require('express');
+const db = require('../db');
 
 const app = express();
 app.use(express.static('public'));
 const port = process.env.PORT || 3002;
 
+// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`Server started on Port ${port}`));
+
+app.get('/:productId', (req, res) => {
+  const { productId } = req.params;
+
+  db.getProduct(productId)
+    .then((product) => {
+      res.status(200).json(product);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+app.get('/api/quantity/:productId&:color&:size&:location', (req, res) => {
+  // eslint-disable-next-line object-curly-newline
+  const { productId, color, size, location } = req.params;
+  db.getQuantity(productId, color, size, location)
+    .then((quantity) => {
+      res.status(200).send(quantity.toString());
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
