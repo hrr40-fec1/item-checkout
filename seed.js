156b5cf2d1eb2e6f7f2c0a3f0e45080ffa2bcfb7
AndrewTjandra <andrew.tjandra@gmail.com>
2019-08-23 08:54:32 -0700

    Implement AirBNB style guide, resolved lint problems

--- seed.js
+++ seed.js
@@ -1,18 +1,19 @@
 const faker = require('faker');
 const db = require('mongoose');
-require('mongoose-currency').loadType(db);
-const Currency = db.Types.Currency;
 
-///////////////////////////
-//  Database Connection  //
-///////////////////////////
-db.connect('mongodb://localhost:27017/ontarget', {useNewUrlParser: true});
+const { Currency } = db.Types;
+require('mongoose-currency').loadType(db);
+
+/*
+  Database Connection
+*/
+db.connect('mongodb://localhost:27017/ontarget', { useNewUrlParser: true });
 
 db.connection.on('connected', () => {
   console.log('connected to MongoDB');
 });
 
-db.connection.on('error', err => {
+db.connection.on('error', (err) => {
   console.log('Error occurred: ', err);
 });
 
@@ -24,28 +25,29 @@ const sizes = ['Small', 'Medium', 'Large', 'XL', 'XXL'];
 const colors = [
   {
     color: 'red',
-    colorUrl: 'http://google.com'
+    colorUrl: 'http://google.com',
   },
   {
     color: 'blue',
-    colorUrl: 'http://google.com'
+    colorUrl: 'http://google.com',
   },
   {
     color: 'green',
-    colorUrl: 'http://google.com'
+    colorUrl: 'http://google.com',
   },
   {
     color: 'orange',
-    colorUrl: 'http://google.com'
-  }
+    colorUrl: 'http://google.com',
+  },
 ];
 
-/////////////////////////
-//  Schema Definition  //
-/////////////////////////
-//Defining review schema to be used as a subdocument in product schema
+/*
+  Schema Definition
+*/
+
+// Defining review schema to be used as a subdocument in product schema
 const reviewSchema = db.Schema({
-  rating: Number
+  rating: Number,
 });
 
 const productSchema = db.Schema({
@@ -54,12 +56,12 @@ const productSchema = db.Schema({
   price: { type: Currency },
   size: [String],
   color: {},
-  reviews: [reviewSchema]
+  reviews: [reviewSchema],
 });
 
 const locationSchema = db.Schema({
   storeId: Number,
-  name: String
+  name: String,
 });
 
 const inventorySchema = db.Schema({
@@ -67,142 +69,133 @@ const inventorySchema = db.Schema({
   size: String,
   color: String,
   storeId: Number,
-  quantity: Number
+  quantity: Number,
 });
 
-////////////////////////
-//  Model Definition  //
-////////////////////////
+/*
+  Model Definition
+*/
 const Products = db.model('products', productSchema);
 const Locations = db.model('locations', locationSchema);
 const Inventory = db.model('inventory', inventorySchema, 'inventory');
 
-//////////////////////////////////
-//  Clear Existing Collections  //
-//////////////////////////////////
-const removeExistingItems = () => {
-  return new Promise((resolve, reject) => {
-    const removedItems = [];
-    removedItems.push(Products.deleteMany({}));
-    removedItems.push(Locations.deleteMany({}));
-    removedItems.push(Inventory.deleteMany({}));
+/*
+  Clear Existing Collections
+*/
+const removeExistingItems = () => new Promise((resolve, reject) => {
+  const removedItems = [];
+  removedItems.push(Products.deleteMany({}));
+  removedItems.push(Locations.deleteMany({}));
+  removedItems.push(Inventory.deleteMany({}));
 
-    Promise.all(removedItems)
-      .then(() => {
-        resolve();
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
-};
+  Promise.all(removedItems)
+    .then(() => {
+      resolve();
+    })
+    .catch((err) => {
+      reject(err);
+    });
+});
 
-////////////////////////////////
-//  Seed Products Collection  //
-////////////////////////////////
-const seedProductCollection = () => {
-  return new Promise ((resolve, reject) => {
-    const createdItems = [];
-    //  Loop to Create 100 product Ids
-    for (let i = 1; i <= 100; i++) {
-      //  Create possible product colors
-      const availableColors = colors.slice();
-      const productColors = [];
-      randomColors = faker.random.number({min: 1, max: colors.length});
+/*
+  Seed Products Collection
+*/
+const seedProductCollection = () => new Promise((resolve, reject) => {
+  const createdItems = [];
+  //  Loop to Create 100 product Ids
+  for (let i = 1; i <= 100; i += 1) {
+    //  Create possible product colors
+    const availableColors = colors.slice();
+    const productColors = [];
+    let randomColors = faker.random.number({ min: 1, max: colors.length });
 
-      while (randomColors > 0) {
-        let randIndex = faker.random.number({min: 0, max: randomColors - 1});
-        productColors.push(availableColors[randIndex]);
-        availableColors.splice(randIndex, 1);
-        randomColors--;
-      }
-
-      //  Construct product object
-      const product = {
-        productId: i,
-        name: faker.commerce.productName(),
-        price: faker.commerce.price(0.01, 50.00),
-        size: sizes,
-        color: productColors,
-        reviews: []
-      };
-
-      //  Create product
-      createdItems.push(Products.create(product));
+    while (randomColors > 0) {
+      const randIndex = faker.random.number({ min: 0, max: randomColors - 1 });
+      productColors.push(availableColors[randIndex]);
+      availableColors.splice(randIndex, 1);
+      randomColors -= 1;
     }
 
-    Promise.all(createdItems)
-      .then((items) => {
-        resolve(items);
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
-};
+    //  Construct product object
+    const product = {
+      productId: i,
+      name: faker.commerce.productName(),
+      price: faker.commerce.price(0.01, 50.00),
+      size: sizes,
+      color: productColors,
+      reviews: [],
+    };
 
-//////////////////////
-//  Create Reviews  //
-//////////////////////
-const createReviews = function(products) {
-  return new Promise((resolve, reject) => {
-    const updatedReviews = [];
+    //  Create product
+    createdItems.push(Products.create(product));
+  }
 
-    //  For each Product
-    for (let i = 0; i < products.length; i++) {
-      const product = products[i];
-      //  Generate random number of reviews (max 100)
-      let numOfReviews = faker.random.number({min: 1, max: 100});
-      for (let i = 0; i < numOfReviews; i++) {
-        //  create n reviews
-        //  rating: random # out of 5
-        let rating = faker.random.number({min: 1, max: 5});
-        product.reviews.push({rating: rating});
-      }
-      updatedReviews.push(product.save());
+  Promise.all(createdItems)
+    .then((items) => {
+      resolve(items);
+    })
+    .catch((err) => {
+      reject(err);
+    });
+});
+
+/*
+  Create Reviews
+*/
+const createReviews = (products) => new Promise((resolve, reject) => {
+  const updatedReviews = [];
+
+  //  For each Product
+  for (let i = 0; i < products.length; i += 1) {
+    const product = products[i];
+    //  Generate random number of reviews (max 100)
+    const numOfReviews = faker.random.number({ min: 1, max: 100 });
+    for (let j = 0; j < numOfReviews; j += 1) {
+      //  create n reviews
+      //  rating: random # out of 5
+      const rating = faker.random.number({ min: 1, max: 5 });
+      product.reviews.push({ rating });
     }
+    updatedReviews.push(product.save());
+  }
 
-    Promise.all(updatedReviews)
-      .then((results) => {
-        resolve('Reviews Created');
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
-};
+  Promise.all(updatedReviews)
+    .then(() => {
+      resolve('Reviews Created');
+    })
+    .catch((err) => {
+      reject(err);
+    });
+});
 
-////////////////////////////////
-//  Seed Location Collection  //
-////////////////////////////////
+/*
+  Seed Location Collection
+*/
 //  Create two locations
-const seedLocationCollection = () => {
-  return new Promise((resolve, reject) => {
-    const createdLocations = [];
-    for (let i = 1; i <= 2; i++) {
-      let location = {
-        storeId: i,
-        name: faker.address.city()
-      };
+const seedLocationCollection = () => new Promise((resolve, reject) => {
+  const createdLocations = [];
+  for (let i = 1; i <= 2; i += 1) {
+    const location = {
+      storeId: i,
+      name: faker.address.city(),
+    };
 
-      createdLocations.push(Locations.create(location));
-    }
+    createdLocations.push(Locations.create(location));
+  }
 
-    Promise.all(createdLocations)
-      .then((locations) => {
-        resolve(locations);
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
-};
+  Promise.all(createdLocations)
+    .then((locations) => {
+      resolve(locations);
+    })
+    .catch((err) => {
+      reject(err);
+    });
+});
 
-
-////////////////////////////////
-// Seed Inventory Collection  //
-////////////////////////////////
-const seedInventoryCollection = function(products, locations) {
+/*
+ Seed Inventory Collection
+*/
+const seedInventoryCollection = (products, locations) => {
   const createdInventory = [];
 
   return new Promise((resolve, reject) => {
@@ -215,13 +208,13 @@ const seedInventoryCollection = function(products, locations) {
           //  ForEach Location
           locations.forEach((storeId) => {
             //  Create Record with random number (max 15)
-            let quantity = faker.random.number({min: 0, max: 15});
-            let item = {
+            const quantity = faker.random.number({ min: 0, max: 15 });
+            const item = {
               productId: product.productId,
-              size: size,
+              size,
               color: color.color,
               storeId: storeId.storeId,
-              quantity: quantity
+              quantity,
             };
 
             createdInventory.push(Inventory.create(item));
@@ -241,29 +234,21 @@ const seedInventoryCollection = function(products, locations) {
 };
 
 
-///////////////////////////////////
-// Run Promise Chain to Seed DB  //
-///////////////////////////////////
+/*
+  Run Promise Chain to Seed DB
+ */
 removeExistingItems()
-  .then(() => {
-    return seedProductCollection();
-  })
-  .then(() => {
-    return seedLocationCollection();
-  })
+  .then(() => seedProductCollection())
+  .then(() => seedLocationCollection())
   .then(() => {
     Products.find({})
-      .then((products) => {
-        return createReviews(products);
-      });
+      .then((products) => createReviews(products));
   })
   .then(() => {
     Products.find({})
       .then((products) => {
         Locations.find({})
-          .then((locations) => {
-            return seedInventoryCollection(products, locations);
-          });
+          .then((locations) => seedInventoryCollection(products, locations));
       });
   })
   .then(() => {
