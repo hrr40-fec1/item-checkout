e696f85eda131003fcc3a92c0453f3b2de741fd7
Andrew Tjandra <49597796+AndrewTjandra@users.noreply.github.com>
2019-08-26 09:37:40 -0700

    Server Configuration with API Routes (#3)
    * Init Server Config
    
    * Setup Express Server, serving public html file
    
    * Added comment to disable lint on console.log
    
    * Implement Express Server with API routes
    
    * Implement GET routes for products, inventory, location

---
+++ server/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const db = require('../db');
+
+const app = express();
+app.use(express.static('public'));
+const port = process.env.PORT || 3002;
+
+// eslint-disable-next-line no-console
+app.listen(port, () => console.log(`Server started on Port ${port}`));
+
+app.get('/:productId', (req, res) => {
+  const { productId } = req.params;
+
+  db.getProduct(productId)
+    .then((product) => {
+      res.status(200).json(product);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+app.get('/api/quantity/:productId&:color&:size&:storeId', (req, res) => {
+  // eslint-disable-next-line object-curly-newline
+  const { productId, color, size, storeId } = req.params;
+  db.getQuantity(productId, color, size, storeId)
+    .then((quantity) => {
+      res.status(200).send(quantity.toString());
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+app.get('/api/location/:storeId', (req, res) => {
+  // eslint-disable-next-line object-curly-newline
+  const { storeId } = req.params;
+  db.getZipCode(storeId)
+    .then((zipCode) => {
+      res.status(200).send(zipCode.toString());
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
