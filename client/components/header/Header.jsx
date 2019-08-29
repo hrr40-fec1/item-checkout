c9737bfdb363f8385dd1dbc18bb70e64e669785d
AndrewTjandra <andrew.tjandra@gmail.com>
2019-08-28 17:56:05 -0700

    React App Skeleton, work in progress

---
+++ client/components/header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Price from './Price';
+import Reviews from './Reviews';
+
+const Header = ({ price, numOfReviews, reviewAverage }) => (
+  <div>
+    <Price price={price} />
+    <Reviews numOfReviews={numOfReviews} reviewAverage={reviewAverage}/>
+  </div>
+);
+
+Header.propTypes = {
+  price: PropTypes.number,
+  numOfReviews: PropTypes.number,
+  reviewAverage: PropTypes.number,
+};
+
+Header.defaultProps = {
+  price: 0,
+  numOfReviews: 0,
+  reviewAverage: 0,
+};
+
+export default Header;
---
+++ client/components/header/Price.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const Price = ({ price }) => (
+  <div id="price">
+    {price}
+  </div>
+);
+
+Price.propTypes = {
+  price: PropTypes.number,
+};
+
+Price.defaultProps = {
+  price: 0,
+};
+
+export default Price;
