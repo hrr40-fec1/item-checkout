b31031cd86bc4912092bdde9b8ee7d3439334f5b
AndrewTjandra <andrew.tjandra@gmail.com>
2019-08-29 15:21:16 -0700

    Create helper function to retreive inventory info

--- client/helper.js
+++ client/helper.js
@@ -2,7 +2,11 @@ import axios from 'axios';
 
 const helper = {
   getProductInfo: (productId) => axios.get(`/api/product/${productId}`),
+
   getLocationInfo: (storeId) => axios.get(`api/location/${storeId}`),
+
+  getInventoryInfo: (productId, color, size, storeId) => axios.get(`/api/quantity/${productId}&${color}&${size}&${storeId}`),
+
   calcAverageRating: (array) => {
     const total = array.reduce((sum, item) => sum + item.rating, 0);
     return Math.round((total / array.length) * 100) / 100;
