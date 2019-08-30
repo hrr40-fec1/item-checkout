b18a2d51512ddfbcf68a9182276e5cb3496e598d
AndrewTjandra <andrew.tjandra@gmail.com>
2019-08-30 15:44:55 -0700

    Render StorePickup Section

--- client/components/checkout/Checkout.jsx
+++ client/components/checkout/Checkout.jsx
@@ -3,19 +3,21 @@ import PropTypes from 'prop-types';
 import StorePickup from './StorePickup';
 import Delivery from './Delivery';
 
-const Checkout = ({ city, zip }) => (
+const Checkout = ({ availableQuantity, city, zip }) => (
   <div>
-    <StorePickup city={city} />
+    <StorePickup availableQuantity={availableQuantity} city={city} />
     <Delivery zip={zip} />
   </div>
 );
 
 Checkout.propTypes = {
+  availableQuantity: PropTypes.number,
   city: PropTypes.string,
   zip: PropTypes.number,
 };
 
 Checkout.defaultProps = {
+  availableQuantity: 0,
   city: '',
   zip: 0,
 };
--- client/components/checkout/StorePickup.jsx
+++ client/components/checkout/StorePickup.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const StorePickup = () => (
+
+const StorePickup = ({ availableQuantity, city }) => (
   <div>
-    StorePickup
+    <div className="col-left">
+      <div>Pick up today</div>
+      <div>{`at ${city}`}</div>
+      <div>Edit store</div>
+      <div>{`only ${availableQuantity} left`}</div>
+    </div>
+    <div className="col-right">
+      <button type="button">Pick it up</button>
+    </div>
+    <div>Ready within 2 hours with Order Pickup</div>
   </div>
 );
+
+StorePickup.propTypes = {
+  availableQuantity: PropTypes.number,
+  city: PropTypes.string,
+};
+
+StorePickup.defaultProps = {
+  availableQuantity: 0,
+  city: '',
+};
 
 export default StorePickup;
