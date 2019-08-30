00047cabc3d03cb50a69b64292636c87dc3cab47
AndrewTjandra <andrew.tjandra@gmail.com>
2019-08-30 11:44:27 -0700

    Create additional unit tests for color/size sections in details

--- tests/details.component.test.jsx
+++ tests/details.component.test.jsx
@@ -3,7 +3,9 @@ import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
 import Details from '../client/components/details/Details';
 import Colors from '../client/components/details/Colors';
+import ColorSquare from '../client/components/details/ColorSquare';
 import Sizes from '../client/components/details/Sizes';
+import SizeButton from '../client/components/details/SizeButton';
 import Quantity from '../client/components/details/Quantity';
 
 Enzyme.configure({ adapter: new Adapter() });
@@ -25,16 +27,44 @@ describe('React Component Testing - Details', () => {
   });
 
   describe('Color Section', () => {
-    test.todo('Colors are rendered based on number of colors for product');
+    test('Colors are rendered based on number of colors for product', () => {
+      const wrapper = shallow(<Colors />);
+      wrapper.setProps({ colors: [{ color: 'blue' }, { color: 'red' }, { color: 'purple' }] });
+      expect(wrapper.find(ColorSquare)).toHaveLength(3);
+    });
+
+    test('Color squares contain color value with appropriate class', () => {
+      const wrapper = shallow(<ColorSquare />);
+      wrapper.setProps({ color: 'Red' });
+      expect(wrapper.find('span').prop('className')).toBe('square Red');
+      expect(wrapper.find('span').prop('value')).toBe('Red');
+    });
+
     test.todo('Current selection for color is highlighted');
   });
 
   describe('Size Section', () => {
-    test.todo('5 buttons is being rendered');
+    test('Size button component is rendering a button with value', () => {
+      const wrapper = shallow(<SizeButton />);
+      wrapper.setProps({ size: 'M' });
+      expect(wrapper.find('button')).toHaveLength(1);
+      expect(wrapper.find('button').prop('value')).toBe('M');
+    });
+
+    test('Size buttons are being rendered', () => {
+      const wrapper = shallow(<Sizes />);
+      wrapper.setProps({ sizes: ['S', 'M', 'L', 'XL', '2Xl'] });
+      expect(wrapper.find(SizeButton)).toHaveLength(5);
+    });
+
+
     test.todo('Current selection for size is highlighted');
   });
 
   describe('Quanity Section', () => {
-    test.todo('Dropdown Exist');
+    test('Dropdown exist with 99 values', () => {
+      const wrapper = shallow(<Quantity />);
+      expect(wrapper.find('option')).toHaveLength(99);
+    });
   });
 });
