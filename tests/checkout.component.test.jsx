efcb9bf0c4baefab66b67cec19cbaa4fb472c2be
AndrewTjandra <andrew.tjandra@gmail.com>
2019-08-30 16:52:54 -0700

    Wrote unit tests for Store Pickup section

--- tests/checkout.component.test.jsx
+++ tests/checkout.component.test.jsx
@@ -4,6 +4,9 @@ import React from 'react';
 import Checkout from '../client/components/checkout/Checkout';
 import StorePickup from '../client/components/checkout/StorePickup';
 import Delivery from '../client/components/checkout/Delivery';
+import InStock from '../client/components/checkout/InStock';
+import OutOfStock from '../client/components/checkout/OutOfStock';
+
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -19,10 +22,41 @@ describe('React Component Testing - Checkout', () => {
   });
 
   describe('StorePickup Section', () => {
-    test.todo('Button to "Pick it up" is rendered');
-    test.todo('In Store Pickup displaying count of product at location');
-    test.todo('Display Out of stock if quantity is 0');
-    test.todo('Display city name of lcoation');
+    test('Render OutOfStock if quantity is 0', () => {
+      const wrapper = shallow(<StorePickup />);
+      wrapper.setProps({ availableQuantity: 0 });
+      expect(wrapper.find(OutOfStock)).toHaveLength(1);
+    });
+
+    test('Render city in OutOfStock component', () => {
+      const wrapper = shallow(<OutOfStock />);
+      wrapper.setProps({ city: 'Port Ivy' });
+      expect(wrapper.find('.h-text-bold').text()).toBe('Out of stock at Port Ivy');
+    });
+
+    test('Render InStock if quantity is 2', () => {
+      const wrapper = shallow(<StorePickup />);
+      wrapper.setProps({ availableQuantity: 2 });
+      expect(wrapper.find(InStock)).toHaveLength(1);
+    });
+
+    test('Button to "Pick it up" is rendered', () => {
+      const wrapper = shallow(<InStock />);
+      expect(wrapper.find('button')).toHaveLength(1);
+      expect(wrapper.find('button').text()).toBe('Pick it up');
+    });
+
+    test('In Store Pickup displaying city received from prop', () => {
+      const wrapper = shallow(<InStock />);
+      wrapper.setProps({ city: 'Port Ivy' });
+      expect(wrapper.find('.h-text-bold').at(1).text()).toBe('at Port Ivy');
+    });
+
+    test('In Store Pickup displaying count of product at location', () => {
+      const wrapper = shallow(<InStock />);
+      wrapper.setProps({ availableQuantity: 10 });
+      expect(wrapper.find('.h-text-orange').text()).toBe('only 10 left');
+    });
   });
 
   describe('Delivery Section', () => {
