1bdc97fb4af8692c5fb19a99f256eacb8700c498
AndrewTjandra <andrew.tjandra@gmail.com>
2019-09-01 14:02:58 -0700

    Styled Size Section

--- client/components/details/SizeButton.jsx
+++ client/components/details/SizeButton.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  width: 70px;
+  height: 33px;
+  border: 1px solid #a9a9a9;
+  border-radius: 10px;
+  margin: 5px 10px;
+  font-size: 14px;
+
+  ${({ selected }) => selected && `
+    border: 2px solid rgb(0, 131, 0);
+  `}
+
+  &:hover {
+    border: 2px solid rgb(0, 0, 0);
+  }
+`;
 
 const SizeButton = ({ size, currentSelected }) => (
-  <button className={currentSelected === size ? 'size selected' : 'size'} type="button" value={size}>{size}</button>
+  <Button type="button" selected={currentSelected === size} value={size}>{size}</Button>
 );
 
 SizeButton.propTypes = {
