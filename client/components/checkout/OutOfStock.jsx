94726834c86d70d2054a40a0b96d8f1b0e579953
AndrewTjandra <andrew.tjandra@gmail.com>
2019-09-03 12:38:22 -0700

    Implement edit store on OutOfStock component

--- client/components/checkout/OutOfStock.jsx
+++ client/components/checkout/OutOfStock.jsx
@@ -2,23 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const EditStore = styled.a`
+const EditStore = styled.button`
+  padding: 0px;
+  border: 0px;
+  background-color: rgb(240, 240, 240);
   font-size: 12px;
+  font-family: Times New Roman;
+  text-decoration: underline;
 `;
 
-const OutOfStock = ({ city }) => (
+const OutOfStock = ({ city, setStoreId }) => (
   <div className="col-left">
     <div className="h-text-bold">{`Out of stock at ${city}`}</div>
-    <EditStore href="#">Edit store</EditStore>
+    <EditStore onClick={() => setStoreId(window.prompt('Enter New Store Id'))}>Edit store</EditStore>
   </div>
 );
 
 OutOfStock.propTypes = {
   city: PropTypes.string,
+  setStoreId: PropTypes.func,
 };
 
 OutOfStock.defaultProps = {
   city: '',
+  setStoreId: () => {},
 };
 
 export default OutOfStock;
